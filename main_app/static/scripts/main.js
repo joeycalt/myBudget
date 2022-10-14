ad2cd7e7fb12419154230b2c838c53ddcd5d3fc7
joeycalt <joeycalt@yahoo.com>
2022-10-14 13:36:23 -0400

    completed some stretch features

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,5 +1,25 @@
-$(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
+// $(".navbar-burger").click(function () {
+//     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//     $(".navbar-burger").toggleClass("is-active");
+//     $(".navbar-menu").toggleClass("is-active");
+//   });
+
+  $(".counter").each(function () {
+    var $this = $(this),
+      countTo = $this.attr("data-countto");
+    countDuration = parseInt($this.attr("data-duration"));
+    $({ counter: $(this).text()}).animate(
+      {
+        counter: countTo
+      },
+      {
+        duration: countDuration,
+        step: function () {
+          $this.text(Math.round(this.counter * 100 )/100);
+        },
+        complete: function () {
+          $this.text(this.counter);
+        }
+      }
+    );
   });
